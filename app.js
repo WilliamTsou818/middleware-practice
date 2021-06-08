1f0093554d0d0458293573e5a08ca945642fa470
WilliamTsou818 <cccc1589@gamil.com>
2021-06-08 16:33:16 +0800

    feat: add middleware to display request time info

--- app.js
+++ app.js
@@ -4,6 +4,19 @@ const exphbs = require('express-handlebars')
 const methodOverride = require('method-override')
 const app = express()
 const port = 3000
+let requestUnixTime
+
+
+app.use(function logMiddlewareInfo(req, res, next) {
+  requestUnixTime = Date.now()
+  const requestTime = new Date(requestUnixTime).toLocaleString('zh-TW')
+  const method = req.method
+  const url = req.originalUrl
+  console.log(`${requestTime} | ${method} from ${url}`)
+
+  next()
+})
+
 
 app.get('/', (req, res) => {
   res.send('列出全部 Todo')
