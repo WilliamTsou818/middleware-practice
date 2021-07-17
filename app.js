2520c0d64cd9eae72a58298dbb00ccd4364c9ed4
WilliamTsou818 <cccc1589@gmail.com>
2021-07-17 17:17:02 +0800

    modify: simplify time format and match standard code style

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 // app.js
 const express = require('express')
 const exphbs = require('express-handlebars')
-const moment = require('moment')
 const { randomId, logRequestInfo, logResponseInfo } = require('./tools')
 const app = express()
 const port = 3000
@@ -10,10 +9,7 @@ const port = 3000
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 
-
-
-
-app.use(function logMiddlewareInfo(req, res, next) {
+app.use(function logMiddlewareInfo (req, res, next) {
   // handle request message
   const [requestLog, requestUnixTime] = logRequestInfo(req)
   console.log(requestLog)
@@ -46,5 +42,5 @@ app.post('/', (req, res) => {
 })
 
 app.listen(port, () => {
-  console.log(`Server is running on http://localhost:3000`)
+  console.log(`Server is running on http://localhost:${port}`)
 })
--- tools.js
+++ tools.js
@@ -5,10 +5,8 @@ const tools = {
     return Math.floor(Math.random() * 100)
   },
   logRequestInfo: (req) => {
-    let requestUnixTime = Date.now()
-    const requestDate = moment(requestUnixTime).format('YYYY-MM-DD')
-    const requestTime = new Date(requestUnixTime).toLocaleTimeString('zh-TW')
-    const requestTimeLog = requestDate + ' ' + moment(requestTime, ["h:mm:ss A"]).format("HH:mm:ss")
+    const requestUnixTime = Date.now()
+    const requestTimeLog = moment(requestUnixTime).format('YYYY-MM-DD HH:mm:ss')
     const method = req.method
     const url = req.originalUrl
     const requestLog = `${requestTimeLog} | ${method} from ${url}`
@@ -18,9 +16,7 @@ const tools = {
     const method = res.req.method
     const url = res.req.originalUrl
     const responseUnixTime = Date.now()
-    const responseDate = moment(responseUnixTime).format('YYYY-MM-DD')
-    const responseTime = new Date(responseUnixTime).toLocaleTimeString('zh-TW')
-    const responseTimeLog = responseDate + ' ' + moment(responseTime, ["h:mm:ss A"]).format("HH:mm:ss")
+    const responseTimeLog = moment(responseUnixTime).format('YYYY-MM-DD HH:mm:ss')
     const handleDuration = responseUnixTime - requestUnixTime
     return `${responseTimeLog} | ${method} from ${url} | total time: ${handleDuration}ms`
   }
